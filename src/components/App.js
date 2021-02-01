4f9e64f87ec70fcfad2d0dc8b3f23c07a6803960
Noura <contact.nouraloudani@gmail.com>
2021-02-01 17:47:48 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "Header";
+import Article from "Article"
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return <div className="App">
+    <Header />
+    <Article />
+    </div>;
 }
 
 export default App;
